025539fa0db8ef8b92df6f2c5f259dadfc469958
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-12-16 10:46:38 +0700

    Menambahkan Price, Number, Total

--- src/main/java/ws/b/project04/Product.java
+++ src/main/java/ws/b/project04/Product.java
@@ -12,6 +12,9 @@ package ws.b.project04; // berfungsi untuk mengimport package yang ada di dalam 
 public class Product { // berfungsi untuk membuat class dengan nama Product
     private String id; // berfungsi untuk membuat id dengan tipe data String
     private String name; // berfungsi untuk membuat name dengan tipe data String
+    private int price; // berfungsi untuk membuat price dengan tipe data int
+    private int Number; // berfungsi untuk membuat Number dengan tipe data int
+    private int total; // berfungsi untuk membuat total dengan tipe data int
 
     public String getId() { // berfungsi untuk membuat method getId
         return id; // berfungsi untuk mengembalikan nilai id
@@ -25,4 +28,22 @@ public class Product { // berfungsi untuk membuat class dengan nama Product
     public void setName(String name) { // berfungsi untuk membuat method setName
         this.name = name; // berfungsi untuk mengisi nilai name
     } // berfungsi untuk menutup method setName
+    public int getPrice() { // berfungsi untuk membuat method getPrice
+        return price; // berfungsi untuk mengembalikan nilai price
+    } // berfungsi untuk menutup method getPrice
+    public void setPrice(int price) { // berfungsi untuk membuat method setPrice
+        this.price = price; // berfungsi untuk mengisi nilai price
+    } // berfungsi untuk menutup method setPrice
+    public int getNumber() { // berfungsi untuk membuat method getNumber
+        return Number; // berfungsi untuk mengembalikan nilai Number
+    } // berfungsi untuk menutup method getNumber
+    public void setNumber(int number) { // berfungsi untuk membuat method setNumber
+        Number = number; // berfungsi untuk mengisi nilai Number
+    } // berfungsi untuk menutup method setNumber
+    public int getTotal() { // berfungsi untuk membuat method getTotal
+        return price * Number; // berfungsi untuk mengembalikan nilai price * Number
+    } // berfungsi untuk menutup method getTotal
+    public void setTotal(int total) { // berfungsi untuk membuat method setTotal
+        this.total = total; // berfungsi untuk mengisi nilai total
+    } // berfungsi untuk menutup method setTotal
 } // berfungsi untuk menutup class Product
